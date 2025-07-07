310fc8d4f1d0a2a02eb860422477e36dfbf926b1
Marwan <kamalmarwan855@gmail.com>
2025-07-07 06:15:56 +0300

    Implement full auth, tab navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -12,8 +12,11 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import CartButton from "@/components/CartButton";
 import { images, offers } from "@/constants";
+import useAuthStore from "@/store/auth.store";
 
 export default function Index() {
+  const { user } = useAuthStore();
+
   return (
     <SafeAreaView className="flex-1 bg-white">
       <FlatList
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,30 @@
+import useAuthStore from "@/store/auth.store";
+import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
-import "./globals.css";
-import { useFonts } from 'expo-font';
 import { useEffect } from "react";
+import "./globals.css";
 
 export default function RootLayout() {
+  const { isLoading, fetchAuthenticatedUser } = useAuthStore();
+
   const [fontsLoaded, error] = useFonts({
-    "Quicksand-Bold": require('../assets/fonts/Quicksand-Bold.ttf'),
-    "Quicksand-Medium": require('../assets/fonts/Quicksand-Medium.ttf'),
-    "Quicksand-Light": require('../assets/fonts/Quicksand-Light.ttf'),
-    "Quicksand-Regular": require('../assets/fonts/Quicksand-Regular.ttf'),
-    "Quicksand-SemiBold": require('../assets/fonts/Quicksand-SemiBold.ttf'),
+    "Quicksand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "Quicksand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "Quicksand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
+    "Quicksand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "Quicksand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
   });
 
   useEffect(() => {
     if (error) throw error;
     if (fontsLoaded) SplashScreen.hideAsync();
   }, [fontsLoaded, error]);
+
+  useEffect(() => {
+    fetchAuthenticatedUser();
+  }, []);
+
+  if (!fontsLoaded || isLoading) return null;
 
   return <Stack screenOptions={{ headerShown: false }} />;
 }
